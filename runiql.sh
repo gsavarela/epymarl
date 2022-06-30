a07db784e9a9a001541231892c04c8308111549b
gsavarela <guilhermevarela@protonmail.com>
2022-06-30 19:46:12 +0100

    Set configs for alg=iql and gymma=lbforaging
    * Add script to test 5 seeds.
    * Update src/config/algs/iql.yamal and src/config/default.yml

---
+++ runiql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+envs=(15x15-4p-3f 8x8-2p-2f-2s-c 10x10-3p-3f-2s 8x8-2p-2f-c 15x15-4p-5f 15x15-3p-5f 10x10-3p-3f)
+
+for e in "${envs[@]}"
+do
+   for i in {0..4}
+   do
+      python src/main.py --config=iql --env-config=gymma with env_args.key="lbforaging:Foraging-$e-v2" seed=$i &
+      echo "Running with $1 and lbforaging:Foraging-$e-v2 for seed=$i"
+      sleep 2s
+   done
+   wait
+    
+
+done
