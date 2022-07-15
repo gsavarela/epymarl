f9bb8b8573893ada333f93bcfdcc550060f59603
gsavarela <guilhermevarela@protonmail.com>
2022-07-15 20:16:38 +0100

    Config runia2c.sh and runmaa2c.sh for lbforaging
    * TODO: Implement independent rewards.
    * Disable parameter sharing.
    * Configurations runia2c is equivalent to independent learners.
    * Configurations runmaa2c is equivalent to distributed parameters.

--- runia2c.sh
+++ runia2c.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-envs=(8x8-2p-2f-coop-v1 2s-8x8-2p-2f-coop-v1 15x15-4p-3f 15x15-4p-5f-v1 2s-10x10-3p-3f-v1 10x10-3p-3f-v1)
+# envs=(8x8-2p-2f-coop-v1 2s-8x8-2p-2f-coop-v1 15x15-4p-3f 15x15-4p-5f-v1 2s-10x10-3p-3f-v1 10x10-3p-3f-v1)
+# control, control+coop, partial_obs, partial_obs+coop, coop
+envs=(15x15-3p-5f-v2 15x15-3p-5f-coop-v2 2s-15x15-3p-5f-v2 2s-15x15-3p-5f-coop-v2)
 seeds=(291174067 392184168 402285178 493194269 503295279)
 
 for e in "${envs[@]}"
 do
-   for i in ${seeds[@]}
-   do
-      python src/main.py --config=ia2c --env-config=gymma with env_args.key="lbforaging:Foraging-$e" seed=$i &
-      echo "Running with ia2c and lbforaging:Foraging-$e for seed=$i"
-      sleep 2s
-   done
-   wait
+    for i in {1..5}
+    do
+        python src/main.py --config=ia2c_ns --env-config=gymma with env_args.key="lbforaging:Foraging-$e" hidden_dim=64 &
+        echo "Running with ia2c_ns and lbforaging:Foraging-$e"
+        sleep 2s
+    done
+    wait
 done
--- runmaa2c.sh
+++ runmaa2c.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # envs=(8x8-2p-2f-coop-v1 2s-8x8-2p-2f-coop-v1 15x15-4p-3f 15x15-4p-5f-v1 2s-10x10-3p-3f-v1 10x10-3p-3f-v1)
-envs=(8x8-2p-2f-coop-v1 2s-8x8-2p-2f-coop-v1 15x15-4p-3f-v1 15x15-4p-5f-v1 15x15-3p-5f-v1 2s-10x10-3p-3f-v1 10x10-3p-3f-v1)
-seeds=(291174067 392184168 402285178 493194269 503295279)
+# envs=(8x8-2p-2f-coop-v1 2s-8x8-2p-2f-coop-v1 15x15-4p-3f-v1 15x15-4p-5f-v1 15x15-3p-5f-v1 2s-10x10-3p-3f-v1 10x10-3p-3f-v1)
+
+# control, control+coop, distributed, distributed+coop
+envs=(15x15-3p-5f-v2 15x15-3p-5f-coop-v2 15x15-3p-5f-share-v2 15x15-3p-5f-coop-share-v2)
+# seeds=(291174067 392184168 402285178 493194269 503295279)
 
 for e in "${envs[@]}"
 do
-   for i in ${seeds[@]}
-   do
-      python src/main.py --config=maa2c --env-config=gymma with env_args.key="lbforaging:Foraging-$e" seed=$i &
-      echo "Running with maa2c and lbforaging:Foraging-$e for seed=$i"
-      sleep 2s
-   done
-   wait
+    for i in {1..5}
+    do
+        python src/main.py --config=maa2c_ns --env-config=gymma with env_args.key="lbforaging:Foraging-$e" hidden_dim=128 &
+        echo "Running with maa2c_ns and lbforaging:Foraging-$e"
+        sleep 2s
+    done
+    wait
 done
