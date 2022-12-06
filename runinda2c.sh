7d3e000e5208c3ac8f73c4098e20741077dae2a5
gsavarela <guilhermevarela@protonmail.com>
2022-12-06 20:36:27 +0000

    Set ntwa2c and inda2c scripts for sanity check
    Set equivalent default configurations for both models.
    Results should be similar.

--- runinda2c.sh
+++ runinda2c.sh
@@ -6,8 +6,8 @@ for e in "${envs[@]}"
 do
     for i in {1..5}
     do
-        python src/main.py --config=inda2c_shl --env-config=gymma with env_args.key="lbforaging:Foraging-$e" hidden_dim=128 &
-        echo "Running with inda2c_shl and lbforaging:Foraging-$e"
+        python src/main.py --config=inda2c --env-config=gymma with env_args.key="lbforaging:Foraging-$e" hidden_dim=64 &
+        echo "Running with inda2c and lbforaging:Foraging-$e"
         sleep 2s
     done
     wait
