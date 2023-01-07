73205e850e6d57eb3f76eec606f1a15b869cb1f0
Guilherme Varela <guilhermevarela@protonmail.com>
2023-01-07 09:42:29 +0000

    Update mongo_plots and add mongo_t_test scripts

--- analysis/mongo_plots.py
+++ analysis/mongo_plots.py
@@ -630,10 +630,19 @@ def main3(
     # QUERY_IDS = [85, 93, 99] # 'hp_grp_12'
     # QUERY_IDS = [86, 92, 105] # 'hp_grp_13'
     # QUERY_IDS = [87, 97, 102] # 'hp_grp_14'
-    QUERY_IDS = [91, 98, 64] # 'hp_grp_15'
+    # QUERY_IDS = [91, 98, 104] # 'hp_grp_15' < 10.14
+    # QUERY_IDS = [106, 113, 119] # 'hp_grp_16'
+    # QUERY_IDS = [107, 112, 125] # 'hp_grp_17'
+    # QUERY_IDS = [108, 118, 124] # 'hp_grp_18'
+    # QUERY_IDS = [114, 121, 127] # 'hp_grp_19'
+    # QUERY_IDS = [109, 115, 120] # 'hp_grp_20'
+    QUERY_IDS = [110, 116, 126] # 'hp_grp_21' < 10.79
+    # QUERY_IDS = [111, 123, 129] # 'hp_grp_22'
+    # QUERY_IDS = [117, 122, 128] # 'hp_grp_23'
+    # QUERY_IDS = [132, 140, 142] # 'hp_grp_26'
     QUERIES.append({
         # hyper_group exploration
-        'label': "hp_grp_15",
+        'label': "hp_grp_21",
         'query_config': {
             'config.env_args.key': ENV,
             'config.name': ALGO_ID,
@@ -652,11 +661,19 @@ def main3(
             # 'config.hypergroup': 'hp_grp_12',
             # 'config.hypergroup': 'hp_grp_13',
             # 'config.hypergroup': 'hp_grp_14',
-            'config.hypergroup': 'hp_grp_15',
-            'config.networked_edges': 2,
+            # 'config.hypergroup': 'hp_grp_15',
+            # 'config.hypergroup': 'hp_grp_17',
+            # 'config.hypergroup': 'hp_grp_18',
+            # 'config.hypergroup': 'hp_grp_19',
+            # 'config.hypergroup': 'hp_grp_20',
+            'config.hypergroup': 'hp_grp_21',
+            # 'config.hypergroup': 'hp_grp_22',
+            # 'config.hypergroup': 'hp_grp_23',
+            # 'config.hypergroup': 'hp_grp_26',
+            'config.networked_edges': 3,
             # 'config.networked_rounds': 1,
-            # 'config.networked_rounds': 5,
-            'config.networked_rounds': 10,
+            'config.networked_rounds': 5,
+            # 'config.networked_rounds': 10,
             'config.networked_interval': 1,
             # 'config.networked_interval': 5
             # 'config.networked_interval': 10,
@@ -732,6 +749,7 @@ def main3(
     title = ''
     steps = defaultdict(list)
     results = defaultdict(list)
+
     for query in QUERIES:
         experiments = loader.find({
             '$and': [
