483702202dd6fe118b2e85eede9ae195d6d98da1
Guilherme Varela <guilhermevarela@protonmail.com>
2023-01-07 09:18:56 +0000

    Update ntwql run script for RWARE

--- runntwql.sh
+++ runntwql.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-envs=(15x15-3p-5f)
+# envs=(15x15-3p-5f)
+envs=(rware-tiny-4ag)
 
 for e in "${envs[@]}"
 do
    for i in {1..5}
    do
-      python src/main.py --config=ntwql --env-config=gymma with env_args.key="lbforaging:Foraging-$e-v1" &
-      echo "Running ntwql and lbforaging:Foraging-$e-v1"
+      # python src/main.py --config=ntwql --env-config=gymma with env_args.key="lbforaging:Foraging-$e-v1" &
+      # echo "Running ntwql and lbforaging:Foraging-$e-v1"
+      python src/main.py --config=ntwql --env-config=gymma with env_args.key="$e-v1" &
+      echo "Running ntwql and rware:$e-v1"
       sleep 2s
    done
    wait
