931fe7d868c5ef4c3822ebb938aaed3eb74ed024
Guilherme Varela <guilhermevarela@protonmail.com>
2023-01-09 18:53:23 +0000

    Set LBF/iql_ns, LBF/ntwql, and LBF/vdn_ns config

---
+++ scripts/runvdn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+envs=(15x15-3p-5f-v1)
+for e in "${envs[@]}"
+do
+    for i in {1..5}
+    do
+        python src/main.py --config=vdn_ns --env-config=gymma with env_args.key="lbforaging:Foraging-$e" hidden_dim=64 env_args.time_limit=50 &
+        echo "Running with vdn_ns and lbforaging:Foraging-$e"
+        sleep 2s
+    done
+    wait
+done
