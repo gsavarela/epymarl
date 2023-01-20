9dd24a449bf5981772720c5109972267ff3aecd3
Guilherme Varela <guilhermevarela@protonmail.com>
2023-01-20 11:00:57 +0000

    Clear controllers

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -2,12 +2,8 @@ REGISTRY = {}
 
 from .basic_controller import BasicMAC
 from .non_shared_controller import NonSharedMAC
-from .maddpg_controller import MADDPGMAC
-from .single_agent_controller import SAC
 from .decentralized_agent_controller import DAC
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["non_shared_mac"] = NonSharedMAC
-REGISTRY["maddpg_mac"] = MADDPGMAC
-REGISTRY["sac"] = SAC
 REGISTRY["dac"] = DAC
