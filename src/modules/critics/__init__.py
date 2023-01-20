f9715823c9197f65644c0e55241525119d2ddb7a
Guilherme Varela <guilhermevarela@protonmail.com>
2023-01-20 11:16:41 +0000

    Clear modules

--- src/modules/critics/__init__.py
+++ src/modules/critics/__init__.py
@@ -1,29 +1,15 @@
-from .coma import COMACritic
 from .centralV import CentralVCritic
-from .coma_ns import COMACriticNS
 from .centralV_ns import CentralVCriticNS
-from .maddpg import MADDPGCritic
-from .maddpg_ns import MADDPGCriticNS
 from .ac import ACCritic
 from .ac_ns import ACCriticNS
-from .ac_dec import ACCriticDecentralized
-from .ac_baseline import ACCriticBaseline
 from .ac_networked import ACCriticNetworked
-from .centralV_baseline import CentralVCriticBaseline
 
 REGISTRY = {}
 
-REGISTRY["coma_critic"] = COMACritic
 REGISTRY["cv_critic"] = CentralVCritic
-REGISTRY["coma_critic_ns"] = COMACriticNS
 REGISTRY["cv_critic_ns"] = CentralVCriticNS
-REGISTRY["maddpg_critic"] = MADDPGCritic
-REGISTRY["maddpg_critic_ns"] = MADDPGCriticNS
 REGISTRY["ac_critic"] = ACCritic
 REGISTRY["ac_critic_ns"] = ACCriticNS
-REGISTRY["ac_critic_dec"] = ACCriticDecentralized
-REGISTRY["ac_critic_baseline"] = ACCriticBaseline
 REGISTRY["ac_critic_networked"] = ACCriticNetworked
-REGISTRY["cv_critic_baseline"] = CentralVCriticBaseline
 
 
