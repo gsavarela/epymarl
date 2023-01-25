8a2588bb1824f5f2fc8ef0360f32472671f8c83f
gsavarela <guilhermevarela@protonmail.com>
2023-01-25 19:25:51 +0000

    Set scripts config for OFFPOLICY/MPE

---
+++ scripts/runvdn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+envs=(SimpleTag-v0)
+
+for e in "${envs[@]}"
+do
+    for i in {0..4}
+    do
+        python src/main.py --config=vdn_ns --env-config=gymma with env_args.time_limit=25 env_args.key="mpe:$e" env_args.pretrained_wrapper="PretrainedTag" &
+        echo "Running with vdn_ns and mpe-$e"
+        sleep 2s
+    done
+    wait
+done
