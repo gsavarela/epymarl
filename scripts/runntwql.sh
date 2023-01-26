c9eef341c9a1016728115c5beb694ec34ed18dae
gsavarela <guilhermevarela@protonmail.com>
2023-01-26 16:15:29 +0000

    Update search and algs config OFFPOLICY/MPE

--- scripts/runntwql.sh
+++ scripts/runntwql.sh
@@ -4,7 +4,7 @@ for e in "${envs[@]}"
 do
    for i in {1..5}
    do
-      python src/main.py --config=ntwql --env-config=gymma with env_args.key="lbforaging:Foraging-$e-v1" &
+      python src/main.py --config=ntwql --env-config=gymma with env_args.key="lbforaging:Foraging-$e-v1" seed=$i &
       echo "Running ntwql and lbforaging:Foraging-$e-v1"
       sleep 2s
    done
