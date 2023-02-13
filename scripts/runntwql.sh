b4ae0a4453f494c7feabf963fef638b49d9f83b2
Guilherme Varela <guilhermevarela@protonmail.com>
2023-02-13 16:29:17 +0000

    Set runntwql.sh to use random seed

--- scripts/runntwql.sh
+++ scripts/runntwql.sh
@@ -5,8 +5,8 @@ for e in "${envs[@]}"
 do
     for i in {0..4}
     do
-        python src/main.py --config=ntwql --env-config=gymma with env_args.time_limit=25 env_args.key="mpe:$e" env_args.pretrained_wrapper="PretrainedTag" seed="$i" &
-        echo "Running with ntwql and mpe-$e force seed=$i"
+        python src/main.py --config=ntwql --env-config=gymma with env_args.time_limit=25 env_args.key="mpe:$e" env_args.pretrained_wrapper="PretrainedTag" &
+        echo "Running with ntwql and mpe-$e"
         sleep 2s
     done
     wait
