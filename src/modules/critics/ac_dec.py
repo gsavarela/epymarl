750806bfe62f65e8c9d143f329ebb4464e9d650f
Guilherme Varela <guilhermevarela@protonmail.com>
2023-04-20 15:06:57 +0100

    Remove observation alignment from ac_dec

--- src/modules/critics/ac_dec.py
+++ src/modules/critics/ac_dec.py
@@ -5,13 +5,6 @@ from modules.critics.ac_ns import ACCriticNS
 
 class ACCriticDecentralized(ACCriticNS):
     def __init__(self, scheme, args):
-
-        # For consensus to work we standardize the triplets
-        # agents observations are different
-        # self.standardize_observations = \
-        #         ('lbforaging' in self.args.env_args['key'])
-        self.standardize_observations = False
-
         super().__init__(scheme, args)
 
 
@@ -46,35 +39,9 @@ class ACCriticDecentralized(ACCriticNS):
 
         inputs = batch["obs"][:, ts, i].clone()
 
-        # TODO: Complete fruits
-        if self.standardize_observations and i > 0:
-            # Current agent on the first position
-            # aligns inputs and complete observations
-            # OBS: on partially observable settings
-            # this won't work as the fruits also
-            # change locations.
-            inputs = self._align_inputs(inputs, i)
-            # mask = th.logical_and(inputs <= 0, obs >=0)
-            # inputs[mask] = obs[mask]
         return inputs, bs, max_t
 
     def _get_input_shape(self, scheme):
         # observations
         input_shape = scheme["obs"]["vshape"]
         return input_shape
-
-
-    # Aligns observations for lbforaging where
-    # current agent is the first agent triplet.
-    def _align_inputs(self, inputs, nswaps):
-        wo = inputs.shape[-1] - 3 * self.n_agents     # write offset
-        ro = wo + 3
-        for _ in range(nswaps): # perform number of swaps
-            aux = inputs[:, :, wo: wo + 3].clone()
-
-            inputs[:, :, wo: wo + 3] = inputs[:, :, ro: ro + 3].clone()
-            inputs[:, :, ro: ro + 3] = aux
-            
-            wo += 3
-            ro += 3
-        return inputs
