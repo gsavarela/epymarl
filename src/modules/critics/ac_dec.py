e33027ad676c98371a880fdedca7a244a0b56999
Guilherme Varela <guilhermevarela@protonmail.com>
2023-05-22 17:31:42 +0100

    Remove joint_observations parameter

--- src/modules/critics/ac_dec.py
+++ src/modules/critics/ac_dec.py
@@ -5,13 +5,6 @@ from modules.critics.ac_ns import ACCriticNS
 
 class ACCriticDecentralized(ACCriticNS):
     def __init__(self, scheme, args):
-
-        # For consensus to work we standardize the triplets
-        # agents observations are different
-        # self.standardize_observations = \
-        #         ('lbforaging' in self.args.env_args['key'])
-        self.standardize_observations = False
-
         super().__init__(scheme, args)
 
 
@@ -44,47 +37,11 @@ class ACCriticDecentralized(ACCriticNS):
         max_t = batch.max_seq_length if t is None else 1
         ts = slice(None) if t is None else slice(t, t+1)
 
-        if self._joint_observations():
-            inputs = batch["state"][:, ts].clone()
-        else:
-            inputs = batch["obs"][:, ts, i].clone()
+        inputs = batch["obs"][:, ts, i].clone()
 
-        # TODO: Complete fruits
-        if self.standardize_observations and i > 0:
-            # Current agent on the first position
-            # aligns inputs and complete observations
-            # OBS: on partially observable settings
-            # this won't work as the fruits also
-            # change locations.
-            inputs = self._align_inputs(inputs, i)
-            # mask = th.logical_and(inputs <= 0, obs >=0)
-            # inputs[mask] = obs[mask]
         return inputs, bs, max_t
 
     def _get_input_shape(self, scheme):
         # observations
-        if self._joint_observations():
-            input_shape = scheme["state"]["vshape"]
-        else:
-            input_shape = scheme["obs"]["vshape"]
+        input_shape = scheme["obs"]["vshape"]
         return input_shape
-
-
-    # Aligns observations for lbforaging where
-    # current agent is the first agent triplet.
-    def _align_inputs(self, inputs, nswaps):
-        wo = inputs.shape[-1] - 3 * self.n_agents     # write offset
-        ro = wo + 3
-        for _ in range(nswaps): # perform number of swaps
-            aux = inputs[:, :, wo: wo + 3].clone()
-
-            inputs[:, :, wo: wo + 3] = inputs[:, :, ro: ro + 3].clone()
-            inputs[:, :, ro: ro + 3] = aux
-            
-            wo += 3
-            ro += 3
-        return inputs
-
-    def _joint_observations(self):
-        return hasattr(self.args, 'networked') and self.args.networked and \
-            hasattr(self.args, 'networked_joint_observations') and self.args.networked_joint_observations
