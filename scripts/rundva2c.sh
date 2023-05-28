1e015e02162898de453de653f43b6845b38118c5
Guilherme Varela <guilhermevarela@protonmail.com>
2023-05-28 22:40:52 +0100

    Config DVA2C and Gymma for LBF tasks

--- scripts/rundva2c.sh
+++ scripts/rundva2c.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-envs=(10x10-3p-3f-v1 15x15-4p-5f-v1 2s-10x10-3p-3f-v1)
+envs=(10x10-3p-3f-v1 2s-10x10-3p-3f-v1 15x15-3p-5f-v1 2s-15x15-3p-5f-v1 15x15-4p-5f-v1 2s-15x15-4p-5f-v1)
 
+# Control
 for e in "${envs[@]}"
 do
-    for i in {1..3}
+    # Test: Zhang et al., 2018
+    for i in {1..5}
     do
-        python src/main.py --config=dva2c --env-config=gymma with env_args.key="lbforaging:Foraging-$e" &
+        python src/main.py --config=dva2c --env-config=gymma with env_args.key="lbforaging:Foraging-$e" env_args.joint_rewards=False &
         echo "Running with dva2c and lbforaging:Foraging-$e"
         sleep 2s
     done
