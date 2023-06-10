2e11dd4afa510275e9b2d1c5e8cccabee4abb527
Guilherme Varela <guilhermevarela@protonmail.com>
2023-06-10 18:22:18 +0100

    Remove breakpoints

--- src/runners/episode_runner.py
+++ src/runners/episode_runner.py
@@ -95,10 +95,11 @@ class EpisodeRunner:
         cur_stats = self.test_stats if test_mode else self.train_stats
         cur_returns = self.test_returns if test_mode else self.train_returns
         log_prefix = "test_" if test_mode else ""
-        try:
-            cur_stats.update({k: cur_stats.get(k, 0) + env_info.get(k, 0) for k in set(cur_stats) | set(env_info)})
-        except Exception:
-            set_trace()
+
+        cur_stats.update({
+            k: cur_stats.get(k, 0) + env_info.get(k, 0)
+            for k in set(cur_stats) | set(env_info) if 'TimeLimit' not in k
+        })
         cur_stats["n_episodes"] = 1 + cur_stats.get("n_episodes", 0)
         cur_stats["ep_length"] = self.t + cur_stats.get("ep_length", 0)
 
